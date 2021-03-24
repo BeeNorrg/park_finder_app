6fb53637d28b216fb1d2e503da29363786e6b351
LittleMagician <bthaoxaochay@gmail.com>
2021-03-23 21:20:28 -0500

    Get route for login.handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
