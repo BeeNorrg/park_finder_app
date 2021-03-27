e01bc1e2551a39a62da10255f5efd82df73dcbaa
Noah Schlag <noahschlag12@gmail.com>
2021-03-27 13:20:22 -0500

    deleted a route that caused an issue with the deployment screen

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,25 +38,19 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/results', async (req,res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['', 'sportsTypes']],
-    });
-  
-    const users = userData.map((project) => project.get({ plain: true }));
-  console.log(users)
-    res.render('results', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get('/results', (req,res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
   }
-  });
+  res.render('results');
+});
 
 
 
+
+
+
+  
 
 module.exports = router;
