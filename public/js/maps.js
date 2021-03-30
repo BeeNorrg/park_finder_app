ac09ef2557837389333bfba954e413680a8f17c9
Jim Nguyen <jimnguyen220@gmail.com>
2021-03-30 01:28:35 -0500

    moved results to partial, updated homepage, info to maps

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,15 +1,48 @@
-
-const parkCoordinates = [{}]
-
+//function calls  the map, this is called in the map.handlebars when the script on line 10
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 44.977, lng: -93.264 },
-    zoom: 10,
+  //renders map
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 44.9463, lng: -93.1100 },
+    zoom: 11,
   });
-  const marker = new google.maps.Marker({
-    position: 
-      {lat: 44.9231, lng: -93.1085},
+  
 
-    map: map
-  })
-};
+
+  //loops through array and calls addMarker function for every item in array
+  for(let i=0; i < locations.length; i++) {
+    addMarker(locations[i])
+  }
+
+  //creates marker to page
+  function addMarker(coords) {
+  const markers = new google.maps.Marker({
+      position: coords,
+      map: map,
+    });
+  };
+
+
+  console.log(locations)
+
+
+};  
+
+  const locations = [
+    {lat: 44.9231, lng: -93.1085},
+    {lat: 44.9470, lng: -93.1141},
+    {lat: 44.9758, lng: -93.1880},
+    {lat: 44.9222, lng: -93.0721},
+    {lat: 44.9534, lng: -93.1201},
+    {lat: 44.9356, lng: -93.1124},
+    {lat: 44.8474, lng: -92.9953}
+  ];
+//can get data here - trying to push to array
+document.querySelectorAll('#park-btn').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const parkCoords = {
+    lat: event.target.getAttribute("data-lat"),
+    lng: event.target.getAttribute("data-lng")
+  }
+  console.log(parkCoords)
+})
