ebd35790c2f75b96666bcc092f389073238a279c
Jim Nguyen <jimnguyen220@gmail.com>
2021-03-31 18:16:30 -0500

    updated change to homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,17 +12,25 @@ router.get('/', withAuth, async (req, res) => {
       order: [['name', 'ASC']],
     });
     const parkData = await Parks.findAll();
+    const parkAmenitiesData = await ParkAmenities.findAll({
+      include: [{model: Amenities}],
+      attributes: {exclude: ['id', 'amenities_id']},
+  });
 
     const api_key = process.env.API_KEY;
     const maps = `https://maps.googleapis.com/maps/api/js?key=${api_key}&callback=initMap&libraries=&v=weekly`
 
     const users = userData.map((project) => project.get({ plain: true }));
     const parks = parkData.map((project) => project.get({ plain: true }));
+    const parkAmenities = parkAmenitiesData.map((project) => project.get({ plain: true }));
 
     // console.log(parks)
+    // console.log(users)
+    console.log(parkAmenities)
 
     res.render('homepage', {
       logged_in: req.session.logged_in,
+      parkAmenities,
       api_key,
       parks,
       maps,
