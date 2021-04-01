b179bc1e4281b84b75feb4412a28373c6df7abdc
LittleMagician <bthaoxaochay@gmail.com>
2021-03-31 21:15:35 -0500

    Scrolling results and login form shows up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -50,6 +50,8 @@ router.post('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
+
+    res.redirect('/login');
   } else {
     res.status(404).end();
   }
